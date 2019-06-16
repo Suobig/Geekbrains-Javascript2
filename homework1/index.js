dcc40aaf105f4102f5077c820608166d8df31ba8
Suobig <apopovwork@gmail.com>
2019-06-17 01:38:03 +0300

    Закончил дз

--- homework1/index.js
+++ homework1/index.js
@@ -1,16 +1,16 @@
 const goods = [
-    {title: "Shirt", price: 150 },
-    {title: "Socks", price: 50 },
-    {title: "Jacket", price: 350 },
-    {title: "Shoes", price: 250 },
+    {title: "Shirt", price: 150, image: "img/placeholder.png" },
+    {title: "Socks", price: 50, image: "img/placeholder.png" },
+    {title: "Jacket", price: 350, image: "img/placeholder.png" },
+    {title: "Shoes", price: 250, image: "img/placeholder.png" },
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`
+const renderGoodsItem = (item) => {
+    return `<div class="goods-item"><img src='${item.image}' alt='${item.title}'/><h3>${item.title}</h3><p>${item.price}</p></div>`
 }
 
 const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    let goodsList = list.map(item => renderGoodsItem(item));
     document.querySelector('.goods-list').innerHTML = goodsList.join(" ");
 }
 
