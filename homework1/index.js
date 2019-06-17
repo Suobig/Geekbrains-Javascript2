168bf051e27e7beb11ba1c5d1243ffad7fd4342f
Suobig <apopovwork@gmail.com>
2019-06-17 20:36:21 +0300

    Разбор ДЗ на уроке

--- homework1/index.js
+++ homework1/index.js
@@ -1,14 +1,21 @@
+const placeHolderImage = "img/placeholder.png";
+
 const goods = [
-    {title: "Shirt", price: 150, image: "img/placeholder.png" },
-    {title: "Socks", price: 50, image: "img/placeholder.png" },
-    {title: "Jacket", price: 350, image: "img/placeholder.png" },
-    {title: "Shoes", price: 250, image: "img/placeholder.png" },
+    { title: "Shirt", price: 150 },
+    { title: "Socks", price: 50 },
+    { title: "Jacket", price: 350 },
+    { title: "Shoes", price: 250 },
 ];
 
-const renderGoodsItem = (title, price, image) =>
-    `<div class="goods-item"><img src='${image}' alt='${title}'/><h3>${title}</h3><p>${price}</p></div>`
+const renderGoodsItem = (title= "<No Title>", price = 0, image = placeHolderImage) =>
+    `<div class="goods-item">
+        <img src='${image}' alt='${title}'/>
+        <h3>${title}</h3>
+        <p>${price}</p>
+        <button class="button-item-add">Добавить</button>
+    </div>`
 
-const renderGoodsList = (list) => {
+const renderGoodsList = list => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.image));
     document.querySelector('.goods-list').innerHTML = goodsList.join();
 }
