3fba050b67d83270ae0b38247e0d8ec07956e344
Suobig <apopovwork@gmail.com>
2019-06-17 19:16:45 +0300

    Учел замечания

--- homework1/index.js
+++ homework1/index.js
@@ -5,13 +5,12 @@ const goods = [
     {title: "Shoes", price: 250, image: "img/placeholder.png" },
 ];
 
-const renderGoodsItem = (item) => {
-    return `<div class="goods-item"><img src='${item.image}' alt='${item.title}'/><h3>${item.title}</h3><p>${item.price}</p></div>`
-}
+const renderGoodsItem = (title, price, image) =>
+    `<div class="goods-item"><img src='${image}' alt='${title}'/><h3>${title}</h3><p>${price}</p></div>`
 
 const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item));
-    document.querySelector('.goods-list').innerHTML = goodsList.join(" ");
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.image));
+    document.querySelector('.goods-list').innerHTML = goodsList.join();
 }
 
 renderGoodsList(goods);
