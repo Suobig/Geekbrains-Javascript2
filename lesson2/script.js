e81dab85771f71317a4e2ae29a711a97a51b8f21
Suobig <apopovwork@gmail.com>
2019-06-18 22:04:31 +0300

    Тестируем работу классов

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,17 +1,44 @@
-class MenuItem {
-  constructor(color, name) {
-    this.color = color;
-    this.name = name;
-  }
+// function TestOuter(val1, val2) {
+//   const self = this;
 
-  makeRed() {
-    this.color = "red";
-  }
-}
+//   let innerVal = val1;
+//   self.publicVal = val2; 
 
-class MainMenuItem extends MenuItem {
-  makeBlue() {
-    this.color = "blue";
-  }
-}
 
+//   self.report = function() {
+//     innerLevel1Report = function() {
+//       console.log(`Inner level 1 function reporting inner: ${innerVal}`);
+//       console.log(`Inner level 1 function reporting public: ${self.publicVal}`);
+//       privateReport();
+//     };
+
+//     console.log(`Outer function reporting: ${innerVal}`);
+//     console.log(`Outer function reporting public: ${self.publicVal}`);
+//     innerLevel1Report();
+//     privateReport();
+//   }
+
+//   function privateReport() {
+//     console.log(`Private function reporting inner: ${innerVal}`);
+//     console.log(`Private function reporting public: ${self.publicVal}`);
+//   }
+// }
+
+
+// class TestOuter {
+//   #privateVal;
+
+//   constructor(val1, val2) { 
+//     this.#privateVal = val1;
+//     this.publicVal = val2;
+//   }
+
+//   report() { 
+//     console.log(`Outer function reporting private: ${this.#privateVal}`);
+//     console.log(`Outer function reporting public: ${this.publicVal}`);
+//   }
+// }
+
+// const outer = new TestOuter("foo", "bar");
+// outer.report();
+// // outer.privateReport();
