da9e2169e5ea90b56f7dfc6b7d4bfcf2a3278ae5
Suobig <apopovwork@gmail.com>
2019-06-22 22:15:30 +0300

    Создал методы для CartItem и Cart

--- eShop/index.js
+++ eShop/index.js
@@ -89,7 +89,8 @@ class CartItem {
      * @param  {} quantity=1 количество единиц товара (по умолчанию - 1)
      */
     constructor(item, quantity=1) {
-
+        this.item = item;
+        this.quantity = quantity;
     }
     
     /**
@@ -98,32 +99,51 @@ class CartItem {
      * @returns {Boolean} true - если успешно добавлено, иначе false
      */
     add(quantity=1) {
-        
+        this.quantity += quantity;
     }
 
     /**
      * Уменьшает количество товара на указанное число штук. 
      * При этом количество не может быть меньше нуля.
+     * @param {Integer} quantity уменьшаемое количество товара
      * @returns {Boolean} true - если успешно удалено, иначе false
      */
-    remove() {
-        
+    remove(quantity) {
+        this.quantity -= quantity;
+        if (this.quantity < 0) this.quantity = 0;
     }
 }
 
 class Cart {
     constructor() {
-        
+        this.cart = new Map();
+        this.total = 0;
     }
     
     /**
      * Добавляет товар в корзину. Если товар уже существует - добавляет 
      * соответствующее количество.
-     * @param  {CartItem} cartItem добавляемый товар
+     * @param  {GoodsItem} cartItem добавляемый товар
      * @returns {Boolean} true - если успешно добавлено, иначе false
      */
-    add(cartItem) {
-        
+    add(goodsItem, quantity=1) {
+        let cartItem;
+        if (this.cart.has(goodsItem)) {
+            cartItem = this.cart.get(goodsItem);
+            cartItem.add(quantity);
+        } else {
+            cartItem = new CartItem(goodsItem, quantity);
+            this.cart.set(goodsItem, cartItem);
+        }
+        this.recalcTotal();
+    }
+
+    remove(goodsItem, quantity=1) {
+        if (this.cart.has(goodsItem)) {
+            const cartItem = this.cart.get(goodsItem);
+            cartItem.remove(quantity);
+            this.recalcTotal();
+        }
     }
 
     /**
@@ -133,8 +153,10 @@ class Cart {
      */
     // (в данном случае мы в качестве аргумента принимаем GoodsItem, так как
     // нам не важно количество товара в корзине - мы удаляем всю позицию)
-    remove(goodsItem) {
-        
+    delete(goodsItem) {
+        const result = this.cart.delete(goodsItem);
+        if (result) this.recalcTotal();
+        return result;
     }
 
     /**
@@ -142,17 +164,26 @@ class Cart {
      * @returns {Number}
      */
     recalcTotal() {
-       
+        const cartItems = this.getItems();
+        this.total = cartItems.reduce( (total, cartItem) => {
+            return total + cartItem.item.price * cartItem.quantity;
+        }, 0);
     }
 
     /**
      * Ищет позицию в корзине
      * @param  {GoodsItem} goodsItem искомый тип товара
      * @returns {CartItem} возвращает найденный CartItem, если данная позиция 
-     * есть в корзине, иначе возвращает null
+     * есть в корзине, иначе возвращает undefined
      */
     find(goodsItem) {
-        
+        return this.cart.get(goodsItem);
+    }
+    /**
+     * @returns { Array {CartItem} } список об
+     */
+    getItems() {
+        return Array.from(this.cart.values())
     }
 }
 
@@ -160,4 +191,26 @@ const list = new GoodsList();
 list.fetchGoods()
         .then( goods => list.parseGoods(goods) )
         .then( () => list.render() )
-        .catch( error => window.alert(error) );
+        .then( () => testCart())
+
+function testCart() {
+    const item1 = list.goods[0];
+    const item2 = list.goods[1];
+
+    const cart = new Cart();
+    cart.add(item1);
+    console.log(`Check add one: ${cart.total == item1.price}`);
+    cart.add(item1, 2);
+    console.log(`Check add two: ${cart.total == item1.price * 3}`);
+    cart.remove(item1);
+    console.log(`Check remove one: ${cart.total == item1.price * 2}`);
+    cart.remove(item1, 2);
+    console.log(`Check remove two: ${cart.total == 0}`);
+    cart.add(item1, 2);
+    cart.add(item2, 2);
+    console.log(`Check add different: ${cart.total == item1.price * 2 + item2.price * 2}`);
+    cart.remove(item1, 10);
+    console.log(`Check remove more: ${cart.total == item2.price * 2}`);
+    cart.delete(item2);
+    console.log(`Check delete: ${cart.total == 0}`);
+}
