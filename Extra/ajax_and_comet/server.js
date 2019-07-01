5f5bd695f99ebbcfd87d8649cf603f9d0a317120
Suobig <apopovwork@gmail.com>
2019-07-02 01:50:31 +0300

    Продолжаем изучать AJAX

--- Extra/ajax_and_comet/server.js
+++ Extra/ajax_and_comet/server.js
@@ -1,21 +1,39 @@
 var http = require('http');
-var url = require("url");
-var queryString = require("querystring");
+var url = require('url');
+var querystring = require('querystring');
 var static = require('node-static');
-var file = new static.Server('.', {
-  cache: 0
-});
+var file = new static.Server('.');
 
-function accept(req, res) {  
-  if (req.url === '/phones.json') {
-    //Искусственная задержка для наглядности
-    setTimeout(() => {
-      file.serve(req, res);
-    }, 1000);
+function accept(req, res) {
+
+  if (req.url == '/digits') {
+
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'no-cache'
+    });
+
+    var i = 0;
+
+    var timer = setInterval(write, 1000);
+    // write();
+
+    function write() {
+      res.write(new Array(10).join(++i + '') + ' ');
+      if (i == 9) {
+        clearInterval(timer);
+        res.end();
+      }
+
+    }
   } else {
     file.serve(req, res);
   }
 }
+
+
+
+// ----- запуск accept как сервера из консоли или как модуля ------
 
 if (!module.parent) {
   http.createServer(accept).listen(8080);
