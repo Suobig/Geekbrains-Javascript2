039889138181a1269745d433713aff7bba06da27
Suobig <apopovwork@gmail.com>
2019-07-01 15:30:03 +0300

    Отображение изображений для элементов списка

--- eShop/index.js
+++ eShop/index.js
@@ -1,213 +1,6 @@
 const placeHolderImage = "img/placeholder.png";
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-
-
-
-function addEventHandlers(list) {
-    const goodsSearch = document.querySelector('.goods-search');
-    const searchInput = document.querySelector('.search-input');
-    goodsSearch.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const val = searchInput.value;
-        list.filterGoods(val);
-    })
-}
-
-class GoodsItem {
-    constructor(title = None, price, image = placeHolderImage) {
-        this.title = title;
-        this.price = price;
-        this.image = image;
-    }
-
-    render() {
-        return `
-            <div class="goods-item">
-                <img src='${this.image}' alt='${this.title}'/>
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-                <button class="button-item-add">Добавить</button>
-            </div>`
-    }
-}
-
-class GoodsList {
-    constructor() {
-        this.goods = [];
-        this.filteredGoods = [];
-    }
-
-    //Асинхронный запрос на удаленный сервер
-    fetchGoods() {
-        return makeGETRequest(`${API_URL}/catalogData.json`);
-    }
-
-    parseGoods(goods) {
-        const lst = JSON.parse(goods)
-        this.goods = lst.map(good => {
-            return new GoodsItem(good.product_name, good.price);
-        });
-        this.filteredGoods = [...this.goods];
-    }
-
-    render() {
-        const listHtml = this.filteredGoods.reduce((acc, good) => {
-            return acc += good.render();
-        }, '')
-        document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-
-    totalCost() {
-        return this.goods.reduce((total, good) => {
-            return total += good.price;
-        }, 0);
-    }
-
-    filterGoods(value) {        
-        const regex = new RegExp(value, 'gmi');
-        this.filteredGoods = this.goods.filter(good => {
-            return regex.test(good.title);
-        });
-        this.render();
-    }
-}
-
-class CartItem {
-    /**
-     * @param  {GoodItem} item покупаемый товар
-     * @param  {} quantity=1 количество единиц товара (по умолчанию - 1)
-     */
-    constructor(item, quantity = 1) {
-        this.item = item;
-        this.quantity = quantity;
-    }
-
-    /**
-     * Увеличивает количество товара на указанное число штук
-     * @param {Integer} quantity добавляемое количество товара
-     * @returns {Boolean} true - если успешно добавлено, иначе false
-     */
-    add(quantity = 1) {
-        this.quantity += quantity;
-    }
-
-    /**
-     * Уменьшает количество товара на указанное число штук. 
-     * При этом количество не может быть меньше нуля.
-     * @param {Integer} quantity уменьшаемое количество товара
-     * @returns {Boolean} true - если успешно удалено, иначе false
-     */
-    remove(quantity) {
-        this.quantity -= quantity;
-        if (this.quantity < 0) this.quantity = 0;
-    }
-}
-
-class Cart {
-    constructor() {
-        this.cart = new Map();
-        this.total = 0;
-    }
-
-    /**
-     * Добавляет товар в корзину. Если товар уже существует - добавляет 
-     * соответствующее количество.
-     * @param  {GoodsItem} cartItem добавляемый товар
-     * @returns {Boolean} true - если успешно добавлено, иначе false
-     */
-    add(goodsItem, quantity = 1) {
-        let cartItem;
-        if (this.cart.has(goodsItem)) {
-            cartItem = this.cart.get(goodsItem);
-            cartItem.add(quantity);
-        } else {
-            cartItem = new CartItem(goodsItem, quantity);
-            this.cart.set(goodsItem, cartItem);
-        }
-        this.recalcTotal();
-    }
-
-    remove(goodsItem, quantity = 1) {
-        if (this.cart.has(goodsItem)) {
-            const cartItem = this.cart.get(goodsItem);
-            cartItem.remove(quantity);
-            this.recalcTotal();
-        }
-    }
-
-    /**
-     * Удаляет заданный тип товара из корзины
-     * @param  {GoodsItem} goodsItem удаляемый товар
-     * @returns {Boolean} true - если успешно удалено, иначе false
-     */
-    // (в данном случае мы в качестве аргумента принимаем GoodsItem, так как
-    // нам не важно количество товара в корзине - мы удаляем всю позицию)
-    delete(goodsItem) {
-        const result = this.cart.delete(goodsItem);
-        if (result) this.recalcTotal();
-        return result;
-    }
-
-    /**
-     * Вычисляет суммарную стомость товаров в корзине
-     * @returns {Number}
-     */
-    recalcTotal() {
-        const cartItems = this.getItems();
-        this.total = cartItems.reduce((total, cartItem) => {
-            return total + cartItem.item.price * cartItem.quantity;
-        }, 0);
-    }
-
-    /**
-     * Ищет позицию в корзине
-     * @param  {GoodsItem} goodsItem искомый тип товара
-     * @returns {CartItem} возвращает найденный CartItem, если данная позиция 
-     * есть в корзине, иначе возвращает undefined
-     */
-    find(goodsItem) {
-        return this.cart.get(goodsItem);
-    }
-    /**
-     * @returns { Array {CartItem} } список об
-     */
-    getItems() {
-        return Array.from(this.cart.values())
-    }
-}
-
-// const list = new GoodsList();
-// list.fetchGoods()
-//     .then(goods => list.parseGoods(goods))
-//     .then(() => list.render())
-//     .then(() => addEventHandlers(list))  
-//     .then(() => testCart())
-//     .catch(error => console.error(error))
-
-// function testCart() {
-//     const item1 = list.goods[0];
-//     const item2 = list.goods[1];
-
-//     const cart = new Cart();
-//     cart.add(item1);
-//     console.log(`Check add one: ${cart.total == item1.price}`);
-//     cart.add(item1, 2);
-//     console.log(`Check add two: ${cart.total == item1.price * 3}`);
-//     cart.remove(item1);
-//     console.log(`Check remove one: ${cart.total == item1.price * 2}`);
-//     cart.remove(item1, 2);
-//     console.log(`Check remove two: ${cart.total == 0}`);
-//     cart.add(item1, 2);
-//     cart.add(item2, 2);
-//     console.log(`Check add different: ${cart.total == item1.price * 2 + item2.price * 2}`);
-//     cart.remove(item1, 10);
-//     console.log(`Check remove more: ${cart.total == item2.price * 2}`);
-//     cart.delete(item2);
-//     console.log(`Check delete: ${cart.total == 0}`);    
-// }
-
-
 const app = new Vue({
     el: "#app",
     data: {
@@ -243,11 +36,25 @@ const app = new Vue({
                 xhr.open('GET', url);
                 xhr.send();
             });
+        },
+        filter() {        
+            const regex = new RegExp(this.searchLine, 'gmi');            
+            this.filteredGoods = this.goods.filter(good => {
+                return regex.test(good.product_name);
+            });
+        },
+        prepareGoods() {
+            this.goods.forEach(item => {
+                if (!item.image) {
+                    item.image = placeHolderImage;
+                }
+            })
         }
     }, 
     async mounted() {
         const goods = await this.makeGETRequest(`${API_URL}/catalogData.json`);
         this.goods = goods;
+        this.prepareGoods();
         this.filteredGoods = goods;
     }
 })
