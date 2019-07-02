b07df28000bb9ac5a8fc67214a173b35c719af2c
Suobig <apopovwork@gmail.com>
2019-07-02 13:50:26 +0300

    Основы XMLHttpRequest

--- Extra/ajax_and_comet/index.js
+++ Extra/ajax_and_comet/index.js
@@ -1,40 +1,46 @@
-// Синхронные запросы
-
-// //1. Создаем новый объект XMLHttpRequest
-// const xhr = new XMLHttpRequest();
-// //2. Конфигурируем его: GET-запрос на URL 'phones-json'
-// xhr.open('GET', 'phones.json', false);
-// //3. Отсылаем запрос 
-// xhr.send();
-// // ...ждем выполнения запроса
-// //4. Если код ответа сервера не 200, то это ошибка
-// if (xhr.status != 200) {
-//   //обрабатываем ошибку
-//   console.error(`${xhr.status}: ${xhr.statusText}`) //пример: 404: Not Found
-// } else {
-//   // обработка результатов
-//   console.log(xhr.responseText); // текст ответа
-// }
-
-// Асинхронные запросы
-
 function loadPhones() {
-  var xhr = new XMLHttpRequest();
-  const buttonEl = document.querySelector("#button");
-  const resultEl = document.querySelector("#result");
 
-  xhr.open("GET", 'phones.json', true);
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'phones.json', true);
+
+
   xhr.send();
 
-  xhr.onreadystatechange = function() {
-    //Запрос еще не завершен
-    if (xhr.readyState !== 4) return;
-    buttonEl.innerHTML = "Готово!";
 
-    if (xhr.status !== 200) {
-      resultEl.innerHTML = `${xhr.status}: ${xhr.statusText}`;
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    button.innerHTML = 'Готово!';
+
+    if (xhr.status != 200) {
+      // обработать ошибку
+      alert(xhr.status + ': ' + xhr.statusText);
     } else {
-      resultEl.innerHTML = xhr.responseText;
+      // вывести результат
+      let phones = null;
+      try {
+        phones = parsePhones(xhr.responseText);
+      } catch (error) {
+        console.error(`Некорректный ответ ${error.message}`);
+        return;
+      }
+      renderPhones(phones);
     }
+
   }
+
+  function parsePhones(text) {
+    return JSON.parse(text);
+  }
+
+  function renderPhones(phones) {
+    const renderedPhones = phones.map( phone => 
+      `<li>${phone.name}</li>` );
+    // const listEl = document.querySelector('.phoneList');
+    phoneList.innerHTML = renderedPhones.join("");
+  }
+  
+  button.innerHTML = 'Загружаю...';
+  button.disabled = true;
 }
--- Extra/ajax_and_comet/server.js
+++ Extra/ajax_and_comet/server.js
@@ -2,43 +2,30 @@ var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 var static = require('node-static');
-var file = new static.Server('.');
+var file = new static.Server('.', {
+  cache: 0
+});
+
 
 function accept(req, res) {
-
-  if (req.url == '/digits') {
-
-    res.writeHead(200, {
-      'Content-Type': 'text/plain',
-      'Cache-Control': 'no-cache'
-    });
-
-    var i = 0;
-
-    var timer = setInterval(write, 1000);
-    // write();
-
-    function write() {
-      res.write(new Array(10).join(++i + '') + ' ');
-      if (i == 9) {
-        clearInterval(timer);
-        res.end();
-      }
-
-    }
+  
+  if (req.url == '/phones.json') {
+    
+    // искусственная задержка для наглядности
+    setTimeout(function() {
+      file.serve(req, res);
+    }, 2000);
   } else {
     file.serve(req, res);
   }
+
 }
 
 
-
-// ----- запуск accept как сервера из консоли или как модуля ------
+// ------ запустить сервер -------
 
 if (!module.parent) {
   http.createServer(accept).listen(8080);
 } else {
   exports.accept = accept;
 }
-
-console.log('Server running on port 8080');
